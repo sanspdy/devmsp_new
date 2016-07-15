482d7939677f3971b19a2d5fd0e688673fc4390c
Manisha Arora <manaror1@in.ibm.com>
2016-07-15 19:30:30 +0530

    latest

--- public/js/main.js
+++ public/js/main.js
@@ -38,6 +38,25 @@ angular.module('portalControllers', ['ui.bootstrap'])
             }
         });
     })
+
+    .directive('onlyDigits', function () {
+
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function (scope, element, attrs, modelCtrl) {
+                modelCtrl.$parsers.push(function (inputValue) {
+                    if (inputValue == undefined) return '';
+                    var transformedInput = inputValue.replace(/[^0-9]/g, '');
+                    if (transformedInput !== inputValue) {
+                        modelCtrl.$setViewValue(transformedInput);
+                        modelCtrl.$render();
+                    }
+                    return transformedInput;
+                });
+            }
+        };
+    })
      // directive for menu slide bar
     .directive('sidebarDirective', function() {
         return {
