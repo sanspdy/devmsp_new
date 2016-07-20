3816409b617595503b766ef7bd7261c75a542c06
Manisha Arora <manaror1@in.ibm.com>
2016-07-20 15:19:01 +0530

    deleteAllSolution api

--- public/app/DeplArch/deplCtrl.js
+++ public/app/DeplArch/deplCtrl.js
@@ -219,8 +219,8 @@ angular.module('portalControllers').controller('deplCtrl', function ($scope,$loc
 
         $http({
             method: 'POST',
-            url: ' /api/v2/deleteAllSolution',
-            data: $.param({'user': uid, 'soln_name': $scope.deletedSolnNameHybrid}),
+            url: '/api/v2/deleteAllSolution',
+            data: $.param({'uname': uid, 'solnName': $scope.deletedSolnNameHybrid}),
             headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
             //forms user object
         })
