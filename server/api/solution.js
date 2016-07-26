9c3716e1b668d713106479ac970b2254ced300d1
manegiri <kvilliva@in.ibm.com>
2016-07-26 10:15:16 +0530

    latest

--- server/api/solution.js
+++ server/api/solution.js
@@ -2563,7 +2563,7 @@ exports.v2_placeOrder=function(reqst, resp) {
                                                     setTimeout(function () {
                                                         db_insert_app(appnames, solnName, full_token_new,uname,version)
 
-                                                    }, 20000);
+                                                    }, 10000);
 
                                                 }
                                                 catch(err){
